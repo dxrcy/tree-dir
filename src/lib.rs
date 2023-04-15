d1750e04e51bbf1db262f80245265329835a2a5d
darcy <darcy3057@gmail.com>
2023-04-15 12:12:04 +1000

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -7,55 +7,55 @@ const BEND: &str = "└─ "; // Bent line (last branch here)
 
 /// Create tree of directory
 pub fn tree(path: &str) -> String {
-  path.replace("\\", "/") // Replace backslashes
+    path.replace("\\", "/") // Replace backslashes
    + "/\n"  // Add trailing slash to root directory name
    + &branch(path, vec![]).trim()
 }
 
 /// Create single branch of tree
 fn branch(path: &str, layers: Vec<&str>) -> String {
-  let mut output = String::from("");
+    let mut output = String::from("");
 
-  // Get vector of directory
-  let dir = fs::read_dir(path)
-    .expect("Could not read directory")
-    .collect::<Vec<Result<fs::DirEntry, std::io::Error>>>();
+    // Get vector of directory
+    let dir = fs::read_dir(path)
+        .expect("Could not read directory")
+        .collect::<Vec<Result<fs::DirEntry, std::io::Error>>>();
 
-  // Loop over files and directories in path
-  for (i, file) in dir.iter().enumerate() {
-    // If is valid file
-    if let Ok(file) = file {
-      // Check if file is directory
-      let is_dir = file
-        .metadata()
-        .expect("Could not read file or directory metadata")
-        .is_dir();
+    // Loop over files and directories in path
+    for (i, file) in dir.iter().enumerate() {
+        // If is valid file
+        if let Ok(file) = file {
+            // Check if file is directory
+            let is_dir = file
+                .metadata()
+                .expect("Could not read file or directory metadata")
+                .is_dir();
 
-      // Format lines from layers
-      let lines = layers.join("") + if i + 1 < dir.len() { FORK } else { BEND };
+            // Format lines from layers
+            let lines = layers.join("") + if i + 1 < dir.len() { FORK } else { BEND };
 
-      // Print name, with trailing slash if is directory
-      output.push_str(&format!(
-        "{lines}{name}{slash}\n",
-        name = file.file_name().to_str().expect("Could not read file name"),
-        slash = if is_dir { "/" } else { "" },
-      ));
+            // Print name, with trailing slash if is directory
+            output.push_str(&format!(
+                "{lines}{name}{slash}\n",
+                name = file.file_name().to_str().expect("Could not read file name"),
+                slash = if is_dir { "/" } else { "" },
+            ));
 
-      // Check if file is a directory
-      if is_dir {
-        // Add new branch
-        output.push_str(&branch(
-          file.path().to_str().expect("Could not read directory path"),
-          // Create temporary vector for extra layer
-          {
-            let mut layers = layers.clone();
-            layers.push(if i + 1 < dir.len() { VERT } else { "   " });
-            layers
-          },
-        ));
-      }
+            // Check if file is a directory
+            if is_dir {
+                // Add new branch
+                output.push_str(&branch(
+                    file.path().to_str().expect("Could not read directory path"),
+                    // Create temporary vector for extra layer
+                    {
+                        let mut layers = layers.clone();
+                        layers.push(if i + 1 < dir.len() { VERT } else { "   " });
+                        layers
+                    },
+                ));
+            }
+        }
     }
-  }
 
-  output
+    output
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use std::fs;
 use tree::tree;
 
 fn main() {
-  // Get command line arguments
-  let mut args = std::env::args();
-  args.next(); // Skip first arg
+    // Get command line arguments
+    let mut args = std::env::args();
+    args.next(); // Skip first arg
 
-  // Create tree
-  let output = tree(&args.next().unwrap_or(".".to_string()));
+    // Create tree
+    let output = tree(&args.next().unwrap_or(".".to_string()));
 
-  // If filename given in args, write to file, otherwise print
-  if let Some(filename) = args.next() {
-    fs::write(filename, output).expect("Could not write file");
-  } else {
-    println!("{}", output);
-  }
+    // If filename given in args, write to file, otherwise print
+    if let Some(filename) = args.next() {
+        fs::write(filename, output).expect("Could not write file");
+    } else {
+        println!("{}", output);
+    }
 }
